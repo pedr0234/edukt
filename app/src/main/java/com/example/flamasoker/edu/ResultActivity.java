51bb4422750ae10e2336dbaa5c0f8565e5e2249f
ArmandoX99 <joan990511@hotmail.com>
2019-11-23 06:54:47 +0100

    EduKT alpha build V.6.2.4

--- app/src/main/java/com/example/flamasoker/edu/ResultActivity.java
+++ app/src/main/java/com/example/flamasoker/edu/ResultActivity.java
@@ -1,5 +1,8 @@
 package com.example.flamasoker.edu;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,12 +15,25 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        TextView resultLabel = (TextView) findViewById(R.id.resultLabel);
-        TextView totalScoreLabel = (TextView) findViewById(R.id.totalScoreLabel);
+        TextView resultLabel = findViewById(R.id.resultLabel);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel);
+
         int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("QUIZ_DATA", Context.MODE_PRIVATE);
+        int totalScore = sharedPreferences.getInt("TOTAL_SCORE", 0);
+        totalScore += score;
+
         resultLabel.setText(score + " / 5");
+        totalScoreLabel.setText("Total Score : " + totalScore);
+
+        // Update total score.
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("TOTAL_SCORE", totalScore);
+        editor.apply();
     }
 
     public void returnTop(View view) {
+        startActivity(new Intent(getApplicationContext(), StartActivity.class));
     }
 }
